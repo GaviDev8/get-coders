22ed4d047e35faebb60bef2041e9b3f9d7d5721e
Commissioner Gordon <geden11@gmail.com>
2024-06-04 10:10:15 -0700

    added profile pages and css

--- client/src/components/BigBidCard.jsx
+++ client/src/components/BigBidCard.jsx
@@ -1,29 +1,45 @@
-import { CoderProfile, RecruiterProfile, BidJob } from 'react-icons/fa';
+// import { CoderProfile, RecruiterProfile, Job } from 'react-icons/fa';
 
-const BigBidCard = ({ title, imageName, description, siteLink, siteVideo, githubLink }) => {
-  return (
-    <div className="big-card">
-      <img src={`/${profileImage}`} alt={`Image for ${coderProfile}`} />
-      <div className="card-content">
-        <h2>{title}</h2>
-        <p>{description}</p>
-        {siteLink ? (
-          <a href={siteLink} className="btn-primary" id="pageButton">
-            <CoderProfile size={30} color={"white"} /> Visit Site
-          </a>
-        ) : (
-          siteVideo && (
-            <a href={siteVideo} className="btn-primary" id="pageButton">
-              <RecruiterProfile size={30} color={"white"} /> Watch Video
-            </a>
-          )
-        )}
-        <a href={job} className="btn-primary" id="pageButton">
-          <BidJob size={30} color={"white"} /> Github
-        </a>
-      </div>
-    </div>
-  );
-};
+// const BigBidCard = ({ coderProfile, description, siteLink, recruiterProfile, githubLink }) => {
+//   return (
+//     <div className="big-card">
+//       <img src={`/${userImage}`} alt={`Image for ${coderProfile}`} />
+//       <div className="card-content">
+//         <h2>{coderProfile}</h2>
+//         <p>{description}</p>
+//         {coderProfile ? (
+//           <a href={siteLink} className="btn-primary" id="pageButton">
+//             <User size={30} color={"white"} /> I can help with this job!
+//           </a>
+//         ) : (
+//           recruiterProfile && (
+//             <a href={siteLink} className="btn-primary" id="pageButton">
+//               <User size={30} color={"white"} /> I need help 
+//             </a>
+//           )
+//         )}
+//         <a href={job} className="btn-primary" id="pageButton">
+//           <Job size={30} color={"white"} /> the project!
+//         </a>
+        
+//       </div>
+//       <div>
+//       <h2>{recruiterProfile}</h2>
+//         <p>{description}</p>
+//         {recruiterProfile ? (
+//           <a href={siteLink} className="btn-primary" id="pageButton">
+//             <User size={30} color={"white"} /> I need help
+//             </a>
+//         ) : (
+//           recruiterProfile && (
+//             <a href={siteLink} className="btn-primary" id="pageButton">
+//               <User size={30} color={"white"} /> I need help
+//             </a>
+//           )
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default BigBidCard;
+// export default BigBidCard;
