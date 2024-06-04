84a2af03f2eee1f08b2eac2ea3d86d2166c92c71
Connor <cguy33333@gmail.com>
2024-06-04 12:08:26 -0700

    added mutations and queries to the client end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,93 @@
+import { gql } from '@apollo/client';
+
+/* ============== USER MUTATIONS ============== */
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      auth {
+        _id
+        user
+      }
+    }
+  }
+`;
+
+export const BECOME_CONTRACTOR = gql`
+  mutation becomeContractor($username: String!, $changeContractor: Boolean!) {
+    becomeContractor(username: $username, changeContractor: $changeContractor) {
+      user {
+        _id
+        username
+        changeContractor
+      }
+    }
+  }
+`;
+
+/* ============== REVIEW MUTATIONS ============== */
+export const ADD_REVIEW = gql`
+  mutation addReview($userId: ID!, $review: Int!, $reviewText: String!) {
+    addReview(userID: $userID, review: $review, reviewText: $reviewText) {
+      user {
+        _id
+      }
+      ratings {
+        review
+        reviewText
+      }
+    }
+  }
+`;
+
+export const REMOVE_REVIEW = gql`
+  mutation removeReview($userId: ID!, $reviewId: ID!) {
+    removeReview(userID: $userID, reviewId: $reviewId) {
+      user {
+        _id
+      }
+      ratings {
+        _id
+      }
+    }
+  }
+`;
+
+/* ============== JOB MUTATIONS ============== */
+export const ADD_JOB = gql`
+  mutation addJob($username: String!, $jobData: JobInput) {
+    addJob(username: $username, jobData: $jobData) {
+      user {
+        username
+      }
+      jobs {
+        jobData
+      }
+    }
+  }
+`;
+
+export const REMOVE_JOB = gql`
+  mutation removeJob($userId: ID!, $jobId: ID!) {
+    removeJob(userId: $userId, jobId: $jobId) {
+      user {
+        _id
+      }
+      jobs {
+        _id
+      }
+    }
+  }
+`;
