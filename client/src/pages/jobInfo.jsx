f9f3e45ec650d3db5ada4ad984456c634ffe8b23
Erik <rainer.erik70@gmail.com>
2024-06-07 13:54:35 -0700

    i got the data for user and for the job now just need some style for the card

--- client/src/pages/jobInfo.jsx
+++ client/src/pages/jobInfo.jsx
@@ -7,36 +7,28 @@ import { DO_BID } from "../utils/mutations"
 import "./jobInfo.css"
 
 function singleJob() {
-    // const { jobId } = useParams();
-    const jobId = "6660f1da413d2b462a7c1571"
+    const { jobId } = useParams();
     const [formState, setFormState] = useState({ newBid: "" });
     // const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
     const [doBid] = useMutation(DO_BID);
 
-    const { data } = useQuery(QUERY_ME);
-    let userId
-    if (data) {
-        userId = data.me;
-    }
-    console.log(userId)
-    console.log(jobId)
-    const { jobData } = useQuery(GET_SINGLE_JOB, {
-        variables: { jobId },
-    });
-    let jobInfo
-    if (jobData) {
-        jobInfo = jobData.job;
-    }
-    console.log(jobInfo)
+    const { loading: userLoading, data: userData } = useQuery(QUERY_ME);
+    const user = userData?.me || {};
 
+    const { loading: jobLoading, error: jobError, data: jobData } = useQuery(GET_SINGLE_JOB, {
+        variables: { jobId: jobId },
+    });
+
+    const jobInfo = jobData?.job || {};
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
         await doBid({
             variables: {
                 currentBid: parseInt(formState.newBid),
-                currentBider: userId
-            }
+                currentBider: user._id
+            },
+            refetchQueries: [{ query: QUERY_ME }],
         });
         setFormState({ newBid: '' })
     };
@@ -79,38 +71,46 @@ function singleJob() {
         });
 
     }
-
+    if (userLoading) {
+        return <div>Loading...</div>;
+    }
     return (
         <div className="card" id="singleJobContainer">
             <div className="container my-4">
-            <article class="card-body p-5">
-                <div className="row">
-                    <div className="col">
-                        <h1 className="title mb-3">TITLE HERE</h1>
-                        <h2 className="mt-3 mb-3">Current Bid:</h2>
-                        <p className="mt-3 mb-3">📅 Ends: 05/05/05</p>
-                        <p className="mt-3 mb-3">Starting Value:</p>
-                    </div>
-                    <div className="col-sm-6">
-                        <form>
-                            <div className="form-group">
-                                <label htmlFor="newBid">Your Bid</label>
-                                <input type="number" className="form-control" id="newBid" placeholder="Enter your bid" />
-                            </div>
-                            <button type="submit" className="btn btn-primary">Bid Now</button>
-                        </form>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <div className="d-flex justify-content-between">
-                            <ul className="list-unstyled d-flex">
-                                <li>Additional Description</li>
-                                <li>Terms</li>
-                            </ul>
+                <article className="card-body p-5">
+                    <div className="row">
+                        <div className="col">
+                            <h1 className="title mb-3">{jobInfo.title}</h1>
+                            <h2 className="mt-3 mb-3">Current Bid:</h2>
+                            <p className="mt-3 mb-3">📅 Ends: 05/05/05</p>
+                            <p className="mt-3 mb-3">Starting Value:</p>
+                        </div>
+                        <div className="col-sm-6">
+                            <form onSubmit={handleFormSubmit}>
+                                <div className="form-group">
+                                    <label htmlFor="newBid">Your Bid</label>
+                                    <input
+                                        type="number"
+                                        className="form-control"
+                                        id="newBid"
+                                        placeholder="Enter your bid"
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <button type="submit" className="btn btn-primary">Bid Now</button>
+                            </form>
                         </div>
                     </div>
-                </div>
+                    <div className="row">
+                        <div className="col">
+                            <div className="d-flex justify-content-between">
+                                <ul className="list-unstyled d-flex">
+                                    <li>Additional Description</li>
+                                    <li>Terms</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </article>
             </div>
         </div>
